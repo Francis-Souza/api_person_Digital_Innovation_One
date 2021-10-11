e35bc0d3ab86218674f62ee336d1bc7714a035c9
Francis-Souza <francisleisouza@gmail.com>
2021-10-11 14:38:42 -0300

    created person updateById

--- src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controllers/PersonController.java
@@ -44,12 +44,12 @@ public class PersonController {
     public List<PersonDTO> listAll() {
         return personService.listAll();
     }
-//
-//    @PutMapping("/{id}")
-//    @ResponseStatus(HttpStatus.OK)
-//    public MessageResponseDTO update(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
-//        return personService.updateById(id, personDTO);
-//    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public MessageResponseDTO update(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
 //
 //    @DeleteMapping("/{id}")
 //    @ResponseStatus(HttpStatus.NO_CONTENT)
